24b4f51762fae097498613428a169af00efe8d52
Brett Dulan <brett.dulan@outlook.com>
2015-06-20 22:56:00 -0400

    more organization - changed file names

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
